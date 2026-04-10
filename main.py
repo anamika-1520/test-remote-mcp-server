bc28844711f729237c896c3e0d9ad04471ec8740
ANAMIKA <24ms0010@gmail.com>
2026-04-10 18:49:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -127,5 +127,5 @@ def categories():
 
 # Start the server
 if __name__ == "__main__":
-    mcp.run(transport="streamable-http", host="0.0.0.0", port=8000)
-    # mcp.run()
+    #mcp.run(transport="streamable-http", host="0.0.0.0", port=8000)
+    mcp.run()
